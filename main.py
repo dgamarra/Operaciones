57f821896aa8a1c365beef04961f1418c4170a55
dgamarra <dgamarra@continental.edu.pe>
2022-04-11 17:19:53 -0500

    Línea base

---
+++ main.py
@@ -0,0 +1,27 @@
+from src.logica.Operaciones import Operaciones
+
+if __name__ == '__main__':
+    operacion = Operaciones()
+    sumando1 = operacion.ingresoDatos(input("Ingrese el primer sumando  : "))
+    sumando2 = operacion.ingresoDatos(input("Ingrese el segundo sumando : "))
+    resultado = operacion.suma(sumando1, sumando2)
+    print("{0:.2f} + {1:.2f} = {2:.2f}".format(sumando1, sumando2, resultado))
+
+    sumando1="3.5"
+    sumando2=4
+    sumando1 = operacion.ingresoDatos(sumando1)
+    sumando2 = operacion.ingresoDatos(sumando2)
+    resultado = operacion.suma(sumando1, sumando2)
+    print("{0:.2f} + {1:.2f} = {2:.2f}".format(sumando1, sumando2, resultado))
+
+    sumando1="3.8"
+    sumando2=4
+    sumando1 = operacion.ingresoDatos(sumando1)
+    sumando2 = operacion.ingresoDatos(sumando2)
+    resultado = operacion.suma(sumando1, sumando2)
+    print("{0:.2f} + {1:.2f} = {2:.2f}".format(sumando1, sumando2, resultado))
+
+    sumando1="3.10"
+    sumando2=4
+    resultado = operacion.suma(sumando1, sumando2)
+    print("{0:.2f} + {1:.2f} = {2:.2f}".format(sumando1, sumando2, resultado))
---
+++ src/logica/Operaciones.py
@@ -0,0 +1,15 @@
+class Operaciones():
+    def suma(self,sumando1,sumando2):
+        for n in (sumando1, sumando2):
+            if not isinstance(n,int) and not isinstance(n,float):
+                raise TypeError("No es un int ni un float")
+        return sumando1+sumando2
+
+    def ingresoDatos(self,dato):
+        if (not type(dato) is float) or (not type(dato) is int):
+            try:
+                dato=float(dato)
+            except ValueError:
+                print(f"El dato ingresado \"{dato}\" no se puede convertir a float")
+                exit(2)
+        return dato
